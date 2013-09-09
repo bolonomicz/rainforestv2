8c70d22eb3ac2949380896cf9d9189ed8cc6b3cf
Ralph Bolo <ralph.bolo@gmail.com>
2013-09-09 18:40:06 -0400

    creating log in function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rainforestv2::Application.routes.draw do
   
 
+  get "sessions/new"
+
+  get "sessions/create"
+
+  get "sessions/destroy"
+
   resources :products
   resources :users, :only => [:new, :create]
+  resources :sessions, :only => [:new, :create, :destroy]
   root :to => 'products#index'
 
 
